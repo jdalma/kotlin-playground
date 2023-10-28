93f435743294e3f69ebe5c7aeddd00f712c372c6
jeongdalma <jeongdalma@gmail.com>
2023-10-29 01:06:18 +0900

    CompletableFuture 예제 추가

--- src/test/java/reactive/AsyncShop.java
+++ src/test/java/reactive/AsyncShop.java
@@ -1,25 +1,33 @@
 package reactive;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.*;
-import java.util.function.ToDoubleBiFunction;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
+import static reactive.ExchangeService.*;
 
 public class AsyncShop {
 
     public static void delay() {
         try {
             Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final Random random = new Random();
+    public static void randomDelay() {
+        int delay = 500 + random.nextInt(1000);
+        try {
+            Thread.sleep(delay);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -39,6 +47,13 @@ public class AsyncShop {
         public String getPriceOfCode(String product) {
             double price = calculatePrice(product);
             Discount.Code code = Discount.Code.values()[new Random().nextInt(5)];
+            return String.format("%s:%.2f:%s", name, price, code);
+        }
+
+        public String getPriceOfCodeAndRandomDelay(String product) {
+            double price = calculatePrice(product);
+            Discount.Code code = Discount.Code.values()[new Random().nextInt(5)];
+            randomDelay();
             return String.format("%s:%.2f:%s", name, price, code);
         }
 
@@ -97,11 +112,13 @@ public class AsyncShop {
     @Test
     void getPriceAsyncExceptionally() throws ExecutionException, InterruptedException {
         Shop shop = new Shop("test");
-//        자싣 스레드에서 예외 발생
+//        자식 스레드에서 예외 발생
 //        shop.getPriceAsync("Exception");
 
         Future<Double> myFavorite = shop.getPriceAsyncExceptionally("Exception");
-        Assertions.assertThrows(ExecutionException.class, myFavorite::get);
+        assertThatThrownBy(myFavorite::get)
+                .isInstanceOf(ExecutionException.class)
+                .hasMessage("java.lang.RuntimeException: Timeout Exception!!!");
     }
 
     @Test
@@ -167,6 +184,7 @@ public class AsyncShop {
         List<String> strings = futures.stream()
                 .map(CompletableFuture::join)
                 .toList();
+        strings.forEach(System.out::println);
         long duration2 = ((System.nanoTime() - start2) / 1_000_000);
         System.out.println("futures process duration2 " + duration2 + " msecs");
         // futures process duration2 1010 msecs
@@ -177,30 +195,139 @@ public class AsyncShop {
         List<Shop> shops = List.of(
                 new Shop("a"),
                 new Shop("b"),
-                new Shop("c"),
-                new Shop("d"),
-                new Shop("e"),
-                new Shop("f"),
-                new Shop("g"),
-                new Shop("h")
+                new Shop("c")
         );
         final Executor executor = Executors.newFixedThreadPool(shops.size());
         final String product = "iPhone";
 
+        long start1 = System.nanoTime();
         List<String> collect = shops.stream()
                 .map(shop -> shop.getPriceOfCode(product))
                 .map(Quote::parse)
                 .map(Discount::applyDiscount)
                 .toList();
+        long duration1 = ((System.nanoTime() - start1) / 1_000_000);
+        System.out.println("shops stream process " + duration1 + " msecs");
+//        shops stream process 6044 msecs
 
+        long start2 = System.nanoTime();
         List<CompletableFuture<String>> collect1 = shops.stream()
                 .map(shop -> CompletableFuture.supplyAsync(
                         () -> shop.getPriceOfCode(product), executor))
+                .peek( (it) -> System.out.println("step1 : " + it))
                 .map(future -> future.thenApply(Quote::parse))
-                .map(future -> future.thenCompose(quote ->
-                        CompletableFuture.supplyAsync(() -> Discount.applyDiscount(quote), executor)))
+                .peek( (it) -> System.out.println("step2 : " + it))
+                .map(future -> future.thenCompose(quoteFuture ->
+                        CompletableFuture.supplyAsync(() -> {
+                            System.out.println("discount!!!");
+                            return Discount.applyDiscount(quoteFuture);
+                        }, executor)))
+                .peek( (it) -> System.out.println("step3 : " + it))
                 .toList();
 
+//        step1 : java.util.concurrent.CompletableFuture@76b1e9b8[Not completed]
+//        step2 : java.util.concurrent.CompletableFuture@5d0bf09b[Not completed]
+//        step3 : java.util.concurrent.CompletableFuture@563f38c4[Not completed]
+//        step1 : java.util.concurrent.CompletableFuture@54422e18[Not completed]
+//        step2 : java.util.concurrent.CompletableFuture@117159c0[Not completed]
+//        step3 : java.util.concurrent.CompletableFuture@3e27ba32[Not completed]
+//        step1 : java.util.concurrent.CompletableFuture@3b0fe47a[Not completed]
+//        step2 : java.util.concurrent.CompletableFuture@202b0582[Not completed]
+//        step3 : java.util.concurrent.CompletableFuture@235ecd9f[Not completed]
+//        discount!!!
+//        discount!!!
+//        discount!!!
+//        shops completable future process 2015 msecs
+
+        for(CompletableFuture<String> future : collect1) {
+            assertThat(future.isDone()).isEqualTo(false);
+        }
+
+        List<String> list = collect1.stream()
+                .map(CompletableFuture::join)
+                .toList();
+
+        long duration2 = ((System.nanoTime() - start2) / 1_000_000);
+        System.out.println("shops completable future process " + duration2 + " msecs");
+
+        for (int i = 0; i < collect1.size(); i++) {
+            CompletableFuture<String> future = collect1.get(i);
+            assertThat(future.isDone()).isEqualTo(true);
+
+            String e = list.get(i);
+            assertThat(e).isNotEmpty();
+        }
     }
 
+    @Test
+    void futurePrice() {
+        final Shop shop = new Shop("a");
+        final String product = "iPhone";
+        CompletableFuture<Double> doubleCompletableFuture = CompletableFuture
+                .supplyAsync(() -> shop.getPrice(product))
+                .thenCombine(
+                    CompletableFuture.supplyAsync(() -> getRate(Money.EUR, Money.USD))
+                                    .completeOnTimeout(DEFAULT_RATE, 1, TimeUnit.SECONDS),
+                    (price, rate) -> price * rate
+                )
+                .orTimeout(3, TimeUnit.SECONDS);
+        Double result = doubleCompletableFuture.join();
+    }
+
+    @Test
+    void java7_futurePrice() {
+        final Shop shop = new Shop("a");
+        final String product = "iPhone";
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<Double> futureRate = executorService.submit(new Callable<Double>() {
+            @Override
+            public Double call() throws Exception {
+                return getRate(Money.EUR, Money.USD);
+            }
+        });
+        Future<Double> futureResult = executorService.submit(new Callable<Double>() {
+            @Override
+            public Double call() throws Exception {
+                double priceInUSD = shop.getPrice(product);
+                return priceInUSD * futureRate.get();
+            }
+        });
+    }
+
+    @Test
+    void findPricesStream() {
+        List<Shop> shops = List.of(
+                new Shop("a"),
+                new Shop("b"),
+                new Shop("c"),
+                new Shop("d"),
+                new Shop("e")
+        );
+        final String product = "iPhone";
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        long start = System.nanoTime();
+        CompletableFuture[] futures = shops.stream()
+                .map(shop -> CompletableFuture.supplyAsync(() -> shop.getPriceOfCodeAndRandomDelay(product), executorService))
+                .map(future -> future.thenApply(Quote::parse))
+                .map(future -> future.thenCompose(futureQuote ->
+                        CompletableFuture.supplyAsync(() -> Discount.applyDiscount(futureQuote), executorService))
+                )
+                .map(future -> future.thenAccept(it -> System.out.printf("%s (done in %s msecs)\n", it, (System.nanoTime() - start) / 1_000_000)))
+                .toArray(CompletableFuture[]::new);
+
+        CompletableFuture.allOf(futures).join();
+//        첫 번쨰
+//        d price is 68.425 (done in 2777 msecs)
+//        a price is 81.605 (done in 2887 msecs)
+//        e price is 118.61 (done in 2910 msecs)
+//        b price is 73.967 (done in 3169 msecs)
+//        c price is 154.60649999999998 (done in 3350 msecs)
+//        두 번째
+//        c price is 162.22 (done in 2558 msecs)
+//        e price is 135.43200000000002 (done in 2588 msecs)
+//        a price is 132.752 (done in 2762 msecs)
+//        d price is 114.08800000000001 (done in 3188 msecs)
+//        b price is 125.229 (done in 3345 msecs)
+    }
 }
