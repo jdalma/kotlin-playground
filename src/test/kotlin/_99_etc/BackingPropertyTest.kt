fa0a2cbbbd3f7ec9f91dc61eaa9e57816d8c3ed4
hjjeong <hjjeong@wert.co.kr>
2023-11-13 00:09:58 +0900

    백킹 프로퍼티 추가

---
+++ src/test/kotlin/_99_etc/BackingPropertyTest.kt
@@ -0,0 +1,29 @@
+package _99_etc
+
+import io.kotest.matchers.equals.shouldBeEqual
+import org.junit.jupiter.api.Test
+
+
+class LottoNumbers1(val numbers: List<Int>)
+
+class LottoNumbers2(numbers: List<Int>) {
+    private val _numbers: List<Int> = numbers.toList()
+    val numbers: List<Int> get() = _numbers
+}
+
+class BackingPropertyTest {
+
+    @Test
+    fun case() {
+        val numbers = mutableListOf(1, 2, 3, 4, 5)
+        val lottoNumber1 = LottoNumbers1(numbers)
+        numbers.add(6)
+
+        lottoNumber1.numbers.size shouldBeEqual 6
+
+        val lottoNumbers2 = LottoNumbers2(numbers)
+        numbers.add(7)
+
+        lottoNumbers2.numbers.size shouldBeEqual 6
+    }
+}
