84c570d68b63fc8dd340ad7b009275fc545126f6
jeongdalma <jeongdalma@gmail.com>
2023-11-29 22:48:30 +0900

    Job 예제 추가

---
+++ src/main/kotlin/_30_Coroutines/Job.kt
@@ -0,0 +1,18 @@
+package _30_Coroutines
+
+import kotlinx.coroutines.*
+
+suspend fun main(): Unit = coroutineScope {
+    val job = Job()
+    launch(job) {
+        delay(1000, 1)
+        println("Text 1")
+    }
+
+    launch(job) {
+        delay(2000, 1)
+        println("Text 2")
+    }
+    job.children.forEach { it.join() }
+    job.complete()
+}
