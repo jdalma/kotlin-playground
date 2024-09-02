c2b414ba2ebf3420abb355525aa4968792cd18f8
jeongdalma <jeongdalma@gmail.com>
2024-09-02 15:58:18 +0900

    수정 및 문제 추가

--- src/test/kotlin/leetcode_study/word-search.kt
+++ src/test/kotlin/leetcode_study/word-search.kt
@@ -17,7 +17,6 @@ class `word-search` {
     private fun usingBacktracking(board: Array<CharArray>, word: String): Boolean {
         fun dfs(board: Array<CharArray>, visited: Array<BooleanArray>, word: String, position: Position, index: Int): Boolean {
             if (index == word.length) return true
-            visited[position.x][position.y] = true
 
             for (move in MOVES) {
                 val next = position + move
@@ -36,6 +35,7 @@ class `word-search` {
 
         for (x in board.indices) {
             for (y in board[x].indices) {
+                visited[x][y] = true
                 if (board[x][y] == word[0] && dfs(board, visited, word, Position(x,y), 1)) {
                     return true
                 }
